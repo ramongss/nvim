826a423a3cceb3015fb564fdaf5d5e9dcf200feb
Ramon G. da Silva <ramongs1406@gmail.com>
2024-02-07 22:42:35 -0300

    Muda a ordem do isort e blue

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,8 +5,8 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.blue,
 				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.blue,
 			},
 		})
 
