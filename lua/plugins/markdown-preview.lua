0466070d4ccc14b2ab0c919fba1a9e9bcdda767e
Ramon G. da Silva <ramongs1406@gmail.com>
2025-03-31 21:35:50 -0300

    Modifica markdown preview plugin

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,6 +1,19 @@
 return {
+  {
     "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = "cd app && npm install",
+    ft = "markdown",
+    lazy = true,
+    keys = { { "gm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" } },
+    config = function()
+      vim.g.mkdp_auto_close = true
+      vim.g.mkdp_open_to_the_world = false
+      vim.g.mkdp_open_ip = "127.0.0.1"
+      vim.g.mkdp_port = "8888"
+      vim.g.mkdp_browser = "firefox"
+      vim.g.mkdp_echo_preview_url = true
+      vim.g.mkdp_page_title = "${name}"
+    end,
+  },
+
 }
